df16230bd9adaf6f04a45fe3b6ce95bc907a3104
Sai Kiran Anugruthi <101090@suneratech.com>
2019-09-13 15:03:33 +0530

    changes

--- vars/SonarAnalysis.groovy
+++ vars/SonarAnalysis.groovy
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
-	def call() {
-		echo "------Sonar Analysis----"
+def call() {
+	echo "------Sonar Analysis----"
 
-		return this
-	}
-=======
-def SonarAnalysis() {
-    echo "---SonarAnalysis---"
-
-    return this
+	return this
 }
->>>>>>> c4dd85d13cb0964c08daba1e23fb439386316c60
